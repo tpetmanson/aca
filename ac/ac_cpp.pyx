109a47f02db67a8406cafb5589d844b36512e096
Timo Petmanson <tpetmanson@gmail.com>
2016-09-26 17:07:13 +0300

    Fixed the importerror

--- ac/ac_cpp.pyx
+++ ac/ac_cpp.pyx
@@ -1,10 +1,9 @@
 # distutils: language = c++
 # distutils: sources = ac/match.cpp ac/node.cpp ac/automaton.cpp
+from libcpp cimport bool
 from libcpp.string cimport string
 from libcpp.vector cimport vector
-from collections import namedtuple
-
-PyMatch = namedtuple('PyMatch', 'start', 'end', 'label')
+import unicodedata
 
 cdef extern from "all.h" namespace "ac":
     cdef cppclass Match:
@@ -19,30 +18,53 @@ cdef extern from "all.h" namespace "ac":
     cdef cppclass Automaton:
         Automaton() except +
         void add(vector[string]&, string)
-        int has_pattern(vector[string]&)
-        int has_prefix(vector[string]&)
+        bool has_pattern(vector[string]&)
+        bool has_prefix(vector[string]&)
         string get_value(vector[string]&, string)
         vector[Match] get_matches(vector[string]&, bool)
+        string str()
 
+
+def normalize_unicode(text):
+    return unicodedata.normalize('NFC', text)
+
+def encode(txt):
+    if isinstance(txt, str):
+        return txt.encode('utf-8')
+    return txt
+
+def decode(txt):
+    if isinstance(txt, bytes):
+        return txt.decode('utf-8')
+    return txt
+
+def encode_list(lst):
+    return [encode]
 
 cdef class PyAutomaton:
-    cdef Automaton cpp_automaton
+    cdef Automaton* cpp_automaton
+
     def __cinit__(self):
-        self.cpp_automaton = Automaton()
+        self.cpp_automaton = new Automaton()
+
+    def __dealloc__(self):
+        del self.cpp_automaton
 
     def add(self, pattern, value):
         self.cpp_automaton.add(pattern, value)
 
     def has_pattern(self, pattern):
-        return bool(self.cpp_automaton.has_pattern(pattern))
+        return self.cpp_automaton.has_pattern(pattern)
 
     def has_prefix(self, prefix):
-        return bool(self.cpp_automaton.has_prefix(prefix))
+        return self.cpp_automaton.has_prefix(prefix)
 
     def get_matches(self, text, exclude_overlaps=True):
         matches = self.cpp_automaton.get_matches(text, exclude_overlaps)
         result = [None]*matches.size()
         for i in range(matches.size()):
-            result[i] = PyMatch(matches[i].get_start(), matches[i].get_end(), matches[i].get_label())
+            result[i] = dict(start=matches[i].get_start(), end=matches[i].get_end(), label=matches[i].get_label())
         return result
 
+    def __str__(self):
+        return decode(self.cpp_automaton.str())
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from Cython.Build import cythonize
 setup(
     ext_modules=cythonize(
         Extension('ac/ac_cpp',
-                  sources=['ac/ac.pyx'],
+                  sources=['ac/ac_cpp.pyx'],
                   language='c++',
                   extra_compile_args=['-std=c++11', '-mmacosx-version-min=10.9'],
                   extra_link_args=['-std=c++11'])
