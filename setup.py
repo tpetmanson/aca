7b0409ab0c9559d0bc2b4367490431ac312a7e9b
Timo Petmanson <tpetmanson@gmail.com>
2016-09-26 15:01:03 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+
+
+setup(
+    ext_modules=cythonize(
+        Extension('ac',
+                  sources=['src/match.cpp', 'src/node.cpp', 'src/automaton.cpp'],
+                  language='c++',
+                  extra_compile_args=['-std=c++11', '-mmacosx-version-min=10.9'],
+                  extra_link_args=['-std=c++11'])
+    )
+)
